1aeadff95ece79b1d779f1c952c03b4858672b8c
Lirlisa <mati_67m@hotmail.com>
2020-08-03 22:44:40 -0400

    ahora lee info desde stdin y hay mejoras de legibilidad

--- problema.py
+++ problema.py
@@ -1,28 +1,29 @@
-archivo = open('input.txt')
-info = []
-for linea in archivo:
-    info.append(linea.strip())
-archivo.close()
+casos = int(input().strip())*2
+n, p ,s = map(int, input().strip().split())
+info = list()
+for caso in range(casos):
+    info.append(input().strip())
 
-casos = int(info[0])*2
-n, p ,s = info[1].split()
-cont = 2
-lis_casos = []
 
-while cont<=casos:
-    matriz = [["" for x in range(len(info[cont+1].split()))] for x in range(len(info[cont].split()))]
-    for i in range(len(info[cont].split())):
-        for j in range(len(info[cont+1].split())):
-            if info[cont].split()[i] == info[cont+1].split()[j]:
+cont = 0
+lis_casos = list()
+
+while cont<casos:
+    matriz = [["" for x in range(s)] for x in range(p)]
+    s1 = info[cont].split()
+    s2 = info[cont+1].split()
+    for i in range(p):
+        for j in range(s):
+            if s1[i] == s2[j]:
                 if i == 0 or j == 0:
-                    matriz[i][j] = info[cont].split()[i]
+                    matriz[i][j] = s1[i]
                 else:
-                    matriz[i][j] = matriz[i-1][j-1] + ' ' + info[cont].split()[i]
+                    matriz[i][j] = matriz[i-1][j-1] + ' ' + s1[i]
             else:
                 matriz[i][j] = max(matriz[i-1][j], matriz[i][j-1], key=len)
     final= matriz[-1][-1]
     lis_casos.append(final)
-    cont+=1
+    cont+=2
 
 
-print (lis_casos)
+print("\n".join(lis_casos))
